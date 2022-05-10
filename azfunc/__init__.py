eb01e3dadb5059f08f53252c3ed70663eeeff16e
Niten Nashiki <niten.nashiki@lightz-inc.com>
2022-05-10 16:22:37 +0900

    update: test を追加

--- azfunc/__init__.py
+++ azfunc/__init__.py
@@ -3,20 +3,35 @@ import logging
 
 import azure.functions as func
 
-logger = logging.getLogger(__name__)
-
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    req_body = req.get_json()
-    logger.debug(req_body)
+    logging.info("Python HTTP trigger function processed a request.")
+
+    try:
+        req_body: dict = req.get_json()
+        logging.debug(req_body)
+    except ValueError:
+        return func.HttpResponse("Invalid body", status_code=400)
+
+    # ここに validation を入れる
 
     if "hoge" in req_body:
         result = req_body["hoge"]
     else:
         result = "success"
 
-    return func.HttpResponse(
-        json.dumps({"result": result}),
-        status_code=200,
-        mimetype="application/json",
-    )
+    is_success_main_process, message = main_process(req_body)
+
+    if is_success_main_process is True:
+        res_body = {"message": message}
+        return func.HttpResponse(
+            json.dumps(res_body),
+            status_code=200,
+            mimetype="application/json",
+        )
+    else:
+        return func.HttpResponse("process error", status_code=500)
+
+
+def main_process(_) -> (bool, str):
+    return True, "success"
--- tests/test_azfunc.py
+++ tests/test_azfunc.py
@@ -1,4 +1,5 @@
 import json
+from unittest.mock import patch
 
 import azure.functions as func
 
@@ -18,7 +19,7 @@ class TestFunction:
         resp = main(req)
         assert resp.status_code == 200
         resp_json = json.loads(resp.get_body().decode())
-        assert resp_json["result"] == "foo"
+        assert resp_json["message"] == "success"
 
     def test_second(self):
         req_body = {}
@@ -32,4 +33,40 @@ class TestFunction:
         resp = main(req)
         assert resp.status_code == 200
         resp_json = json.loads(resp.get_body().decode())
-        assert resp_json["result"] == "success"
+        assert resp_json["message"] == "success"
+
+    def test_req_body_parse_error(self):
+        req_body = {}
+        req_headers = {}
+
+        def raise_exception(*args, **kwargs):
+            raise ValueError
+
+        with patch("azure.functions.HttpRequest.get_json", new=raise_exception):
+            req = func.HttpRequest(
+                method="POST",
+                body=json.dumps(req_body).encode("utf-8"),
+                url="/api/azfunc",
+                headers=req_headers,
+            )
+            resp = main(req)
+            assert resp.status_code == 400
+            assert resp.get_body().decode() == "Invalid body"
+
+    def test_main_process_error(self):
+        req_body = {}
+        req_headers = {}
+
+        def main_process_error(*args, **kwargs):
+            return False, "Fail"
+
+        with patch("azfunc.main_process", new=main_process_error):
+            req = func.HttpRequest(
+                method="POST",
+                body=json.dumps(req_body).encode("utf-8"),
+                url="/api/azfunc",
+                headers=req_headers,
+            )
+            resp = main(req)
+            assert resp.status_code == 500
+            assert resp.get_body().decode() == "process error"
