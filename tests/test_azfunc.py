8bc03555f18ee36150bf00b7e44b6a293757f82d
Niten Nashiki <niten.nashiki@lightz-inc.com>
2022-05-10 14:57:14 +0900

    fmt: テストパラメータがわかりにくいので

--- tests/test_azfunc.py
+++ tests/test_azfunc.py
@@ -7,11 +7,13 @@ from azfunc import main
 
 class TestFunction:
     def test_first(self):
+        req_body = {"hoge": "foo"}
+        req_headers = {}
         req = func.HttpRequest(
             method="POST",
-            body=json.dumps({"hoge": "foo"}).encode("utf-8"),
+            body=json.dumps(req_body).encode("utf-8"),
             url="/api/azfunc",
-            headers={},
+            headers=req_headers,
         )
         resp = main(req)
         assert resp.status_code == 200
@@ -19,11 +21,13 @@ class TestFunction:
         assert resp_json["result"] == "foo"
 
     def test_second(self):
+        req_body = {}
+        req_headers = {}
         req = func.HttpRequest(
             method="POST",
-            body=json.dumps({}).encode("utf-8"),
+            body=json.dumps(req_body).encode("utf-8"),
             url="/api/azfunc",
-            headers={},
+            headers=req_headers,
         )
         resp = main(req)
         assert resp.status_code == 200
